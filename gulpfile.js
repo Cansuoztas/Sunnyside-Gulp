544fe263dd8d241028dacc0695ec894b8a77a5d7
Cansuoztas <fcansuoztas@gmail.com>
2023-01-31 14:39:57 +0300

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,8 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-// const csso = require('gulp-csso')
 var minifyCSS= require('gulp-minify-css');
 const concat =require('gulp-concat')
-// const sourcemaps =require('gulp-sourcemaps')
-
 
 
 
@@ -14,13 +11,16 @@ gulp.task('css',()=>{
     return gulp.src('./sass/**/*.sass')
     .pipe(sass())
     .pipe(minifyCSS())
-
     .pipe(concat('style.css'))
     .pipe(gulp.dest('./Css'))
 })
 
 
+ 
 gulp.task('watch',()=>{
     gulp.watch('./sass/**/*.sass',gulp.series('css'))
 })
+
+
+
 gulp.task('default',gulp.series('watch','css'))
